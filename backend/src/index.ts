6a44b9534ebdaaec8e74f517ec39e4c2a9a8ab28
djmbritt <david@effect.ai>
2022-12-09 22:46:15 +0100

    Fix types and add temp asm token

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,38 @@
 import { PrismaClient } from '@prisma/client';
 import express, { Express, Request, Response } from 'express'
+import axios, {  } from 'axios'
+import cors from 'cors'
 import dotenv from 'dotenv'
 
 
-dotenv.config()
+dotenv.config({
+    debug: true
+})
 const prisma = new PrismaClient()
 const app: Express = express()
+app.use(express.json())
+app.use(cors())
 const port = process.env.PORT ?? 8282
 
 app.get('/', (_req: Request, res: Response) => res.send("🔥"))
+
+app.get('/asmtoken', async (_req: Request, res: Response) => {
+    const assemblyApiTokenUrl = 'https://api.assemblyai.com/v2/realtime/token'
+    try {
+        console.log('Getting ASM token 🔐', process.env.ASSEMBLY_KEY)
+        // const response = await fetch(assemblyApiTokenUrl, {
+        //     headers: { authorization: process.env.ASSEMBLY_KEY }
+        // })
+        const response = await axios.post(assemblyApiTokenUrl, 
+            { expires_in: 3600 }, 
+            { headers: { authorization: process.env.ASSEMBLY_KEY} })
+        const { data } = response
+        res.json(data)
+    } catch (error) {
+        const { response: { status, data } } = error
+        res.status(status).json(data)
+    }
+})
 
 // Example GET
 app.get('/feed', async (_req: Request, res: Response) => {
@@ -35,7 +59,8 @@ app.get('/post',async (req: Request, res: Response) => {
 
 // Example PUT
 app.put('/publish/:id', async (req: Request, res: Response) => {
-    const { id } = req.params
+
+    const { id }: {id?: number} = req.params
     const post = await prisma.post.update({
         where: { id },
         data: { published: true }
@@ -43,10 +68,9 @@ app.put('/publish/:id', async (req: Request, res: Response) => {
     res.json(post)
 })
 
-
 //Example DELETE
 app.delete('/user/:id', async (req: Request, res: Response) => {
-    const { id } = req.params
+    const { id }: { id?: number } = req.params
     const user = await prisma.user.delete({
         where: { id }
     })
