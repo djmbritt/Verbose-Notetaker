e2c95d01b5ffe089080286ae4663258544230f27
djmbritt <david@effect.ai>
2022-12-10 23:47:01 +0100

    Add new chrome ext

--- chrome_ext/README.md
+++ chrome_ext/README.md
@@ -1,21 +1,2 @@
-# <img src="public/icons/icon_48.png" width="45" align="left"> Verbose_notetaker
-
-My Chrome Extension
-
-## Features
-
-- Feature 1
-- Feature 2
-
-## Install
-
-[**Chrome** extension]() <!-- TODO: Add chrome extension link inside parenthesis -->
-
-## Contribution
-
-Suggestions and pull requests are welcomed!.
-
----
-
-This project was bootstrapped with [Chrome Extension CLI](https://github.com/dutiyesh/chrome-extension-cli)
-
+# chrome-audio-capture
+Chrome Audio Capture
---
+++ chrome_ext/options.js
@@ -0,0 +1,251 @@
+function tabCapture() {
+  return new Promise((resolve) => {
+    chrome.tabCapture.capture(
+      {
+        audio: true,
+        video: false,
+      },
+      (stream) => {
+        resolve(stream);
+      }
+    );
+  });
+}
+
+function to16BitPCM(input) {
+  const dataLength = input.length * (16 / 8);
+  const dataBuffer = new ArrayBuffer(dataLength);
+  const dataView = new DataView(dataBuffer);
+  let offset = 0;
+  for (let i = 0; i < input.length; i++, offset += 2) {
+    const s = Math.max(-1, Math.min(1, input[i]));
+    dataView.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true);
+  }
+  return dataView;
+}
+
+function to16kHz(audioData, sampleRate = 44100) {
+  const data = new Float32Array(audioData);
+  const fitCount = Math.round(data.length * (16000 / sampleRate));
+  const newData = new Float32Array(fitCount);
+  const springFactor = (data.length - 1) / (fitCount - 1);
+  newData[0] = data[0];
+  for (let i = 1; i < fitCount - 1; i++) {
+    const tmp = i * springFactor;
+    const before = Math.floor(tmp).toFixed();
+    const after = Math.ceil(tmp).toFixed();
+    const atPoint = tmp - before;
+    newData[i] = data[before] + (data[after] - data[before]) * atPoint;
+  }
+  newData[fitCount - 1] = data[data.length - 1];
+  return newData;
+}
+
+function sendMessageToTab(tabId, data) {
+  return new Promise((resolve) => {
+    chrome.tabs.sendMessage(tabId, data, (res) => {
+      resolve(res);
+    });
+  });
+}
+
+async function startRecord(option) {
+  const stream = await tabCapture();
+
+  if (stream) {
+    // call when the stream inactive
+    stream.oninactive = () => {
+      window.close();
+    };
+
+    try {
+      const recorder = new RecordRTC(stream, {
+        type: 'audio',
+        mimeType: 'audio/webm;codecs=pcm', // endpoint requires 16bit PCM audio
+        recorderType: StereoAudioRecorder,
+        timeSlice: 250, // set 250 ms intervals of data that sends to AAI
+        desiredSampRate: 16000,
+        numberOfAudioChannels: 1, // real-time requires only one channel
+        bufferSize: 4096,
+        audioBitsPerSecond: 128000,
+        ondataavailable: (blob) => {
+          const reader = new FileReader();
+          reader.onload = () => {
+            const base64data = reader.result;
+
+            // audio data must be sent as a base64 encoded string
+            if (socket) {
+              socket.send(JSON.stringify({ audio_data: base64data.split('base64,')[1] }));
+            }
+          };
+          reader.readAsDataURL(blob);
+        },
+      });
+
+      recorder.startRecording();
+    } catch (error) {
+      console.error(error)
+    }
+
+    const audioDataCache = [];
+    const context = new AudioContext();
+    const mediaStream = context.createMediaStreamSource(stream);
+    const recorder = context.createScriptProcessor(0, 1, 1);
+
+    recorder.onaudioprocess = async (event) => {
+      if (!context) return;
+
+      const inputData = event.inputBuffer.getChannelData(0);
+      const output = to16kHz(inputData, context.sampleRate);
+      const audioData = to16BitPCM(output);
+
+      audioDataCache.push(...new Int8Array(audioData.buffer));
+
+      if (audioDataCache.length > 1280) {
+        const audioDataArray = new Int8Array(audioDataCache);
+
+        // Process your audio data here
+        // console.log(audioDataArray);
+
+        // You can pass some data to current tab
+        await sendMessageToTab(option.currentTabId, {
+          type: "FROM_OPTION",
+          data: audioDataArray.length,
+        });
+
+        audioDataCache.length = 0;
+      }
+    };
+
+    // Prevent page mute
+    mediaStream.connect(recorder);
+    recorder.connect(context.destination);
+    mediaStream.connect(context.destination);
+  } else {
+    window.close();
+  }
+}
+
+// Receive data from Current Tab or Background
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const { type, data } = request;
+
+  switch (type) {
+    case "START_RECORD":
+      startRecord(data);
+      break;
+    default:
+      break;
+  }
+
+  sendResponse({});
+});
+
+
+
+// ----------------------------------------------------------------------------
+
+
+// required dom elements
+const buttonEl = document.getElementById('button');
+const messageEl = document.getElementById('message');
+const titleEl = document.getElementById('real-time-title');
+
+// set initial state of application variables
+messageEl.style.display = 'none';
+let isRecording = false;
+let socket;
+let recorder;
+
+// runs real-time transcription and handles global variables
+const run = async () => {
+  if (isRecording) { 
+    if (socket) {
+      socket.send(JSON.stringify({terminate_session: true}));
+      socket.close();
+      socket = null;
+    }
+
+    if (recorder) {
+      recorder.pauseRecording();
+      recorder = null;
+    }
+  } else {
+    const response = await fetch('http://localhost:8282/asmtoken'); // get temp session token from server.js (backend)
+    const data = await response.json();
+
+    if(data.error){
+      alert(data.error)
+    }
+    
+    const { token } = data;
+
+    // establish wss with AssemblyAI (AAI) at 16000 sample rate
+    socket = await new WebSocket(`wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000&token=${token}`);
+
+    // handle incoming messages to display transcription to the DOM
+    const texts = {};
+    socket.onmessage = (message) => {
+      let msg = '';
+      const res = JSON.parse(message.data);
+      texts[res.audio_start] = res.text;
+      const keys = Object.keys(texts);
+      keys.sort((a, b) => a - b);
+      for (const key of keys) {
+        if (texts[key]) {
+          msg += ` ${texts[key]}`;
+        }
+      }
+      messageEl.innerText = msg;
+    };
+
+    socket.onerror = (event) => {
+      console.error(event);
+      socket.close();
+    }
+    
+    socket.onclose = event => {
+      console.log(event);
+      socket = null;
+    }
+
+    socket.onopen = () => {
+      // once socket is open, begin recording
+      messageEl.style.display = '';
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+          recorder = new RecordRTC(stream, {
+            type: 'audio',
+            mimeType: 'audio/webm;codecs=pcm', // endpoint requires 16bit PCM audio
+            recorderType: StereoAudioRecorder,
+            timeSlice: 250, // set 250 ms intervals of data that sends to AAI
+            desiredSampRate: 16000,
+            numberOfAudioChannels: 1, // real-time requires only one channel
+            bufferSize: 4096,
+            audioBitsPerSecond: 128000,
+            ondataavailable: (blob) => {
+              const reader = new FileReader();
+              reader.onload = () => {
+                const base64data = reader.result;
+
+                // audio data must be sent as a base64 encoded string
+                if (socket) {
+                  socket.send(JSON.stringify({ audio_data: base64data.split('base64,')[1] }));
+                }
+              };
+              reader.readAsDataURL(blob);
+            },
+          });
+
+          recorder.startRecording();
+        })
+        .catch((err) => console.error(err));
+    };
+  }
+
+  isRecording = !isRecording;
+  buttonEl.innerText = isRecording ? 'Stop' : 'Record';
+  titleEl.innerText = isRecording ? 'Click stop to end recording!' : 'Click start to begin recording!'
+};
+
+buttonEl.addEventListener('click', () => run());
